f3a7f48666f1bb0bd08b0890a2aab0762c04476e
Mathis Dagbert <144277889+MathDriprio@users.noreply.github.com>
2023-10-23 14:00:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app.secret_key = 'une cle(token) : grain de sel(any random string)'
 
 
 def get_data(api):
-    response = req.get(f"{api}")
+    response = req.get(f"{api}", verify=False)
     if response.status_code == 200:
         return response.json()
     else:
@@ -16,11 +16,23 @@ def get_data(api):
         return []
 
 
-print(get_data("https://node2.adky.net:25286/voiture"))
-@app.route('/clubs/show')
-def show_clubs():
-    clubs = "get_data()"
-    return render_template('clubs/show_clubs.html', clubs=clubs)
+@app.route('/show_voiture')
+def show_voiture():
+    clubs = get_data("http://node2.adky.net:25286/voiture")
+    return render_template('show_voitures.html', clubs=clubs)
+
+@app.route('/page_detail')
+def page_detail():
+    id = request.args.get('id')
+    # clubs = get_data("http://node2.adky.net:25286/voiture")
+    voiture = get_data(f"http://node2.adky.net:25286/voiture?id="+id)
+    return render_template('page_detail.html', voiture=voiture[0])
+
+@app.route('/panier', methods=['GET'])
+def page_panier():
+    panier = get_data("http://node2.adky.net:25286/panier")
+    return render_template('panier.html', panier=panier)
 
 if __name__ == '__main__':
     app.run()
+

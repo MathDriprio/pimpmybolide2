cbf858dc8b7d9964246db6189d14cf63dcbebc31
Mathis Dagbert <144277889+MathDriprio@users.noreply.github.com>
2023-10-27 11:24:10 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for, abort, flash, g
 import requests as req
-import json
+import json, math
 
 
 app = Flask(__name__)
@@ -16,22 +16,52 @@ def get_data(api):
         return []
 
 
+@app.route("/")
+def accueuil():
+    return render_template("acceuil.html")
+
+
 @app.route('/show_voiture')
 def show_voiture():
-    clubs = get_data("http://node2.adky.net:25286/voiture")
-    return render_template('show_voitures.html', clubs=clubs)
+    voitures = get_data("http://node2.adky.net:25286/voiture")
+    return render_template('show_voitures.html', voitures = voitures)
+
 
 @app.route('/page_detail')
 def page_detail():
     id = request.args.get('id')
     # clubs = get_data("http://node2.adky.net:25286/voiture")
-    voiture = get_data(f"http://node2.adky.net:25286/voiture?id="+id)
+    voiture = get_data(f"http://node2.adky.net:25286/voiture?id=" + id)
     return render_template('page_detail.html', voiture=voiture[0])
+
 
 @app.route('/panier', methods=['GET'])
 def page_panier():
     panier = get_data("http://node2.adky.net:25286/panier")
-    return render_template('panier.html', panier=panier)
+    total = 0
+    for car in panier:
+        total+=car["prixVoiture"] * pow(1.1, car["stage"])
+    return render_template('panier.html', panier=panier, total=format(total, '.2f'))
+
+
+@app.route("/panier/delete", methods=["GET"])
+def del_panier():
+    idPanier = request.args.get("id")
+    req.delete(f"http://node2.adky.net:25286/panier?idPanier="+idPanier, verify=False)
+    return redirect("/panier")
+
+@app.route("/panier/add", methods=["GET"])
+def add_panier():
+    idVoiture = request.args.get("idVoiture")
+    stage = request.args.get("stage")
+    if stage is None:
+        stage = 0
+    req.post(f"http://node2.adky.net:25286/panier?idVoiture="+str(idVoiture)+"&stageVoiture="+str(stage), verify=False)
+    return redirect("/panier")
+
+@app.route("/validerPanier")
+def lol():
+    return render_template("validerPanier.html")
 
 if __name__ == '__main__':
     app.run()
--- static/js/script.js
+++ static/js/script.js
@@ -38,3 +38,92 @@ window.addEventListener("scroll", function () {
     goTopBtn.classList.remove("active");
   }
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    let currentImageIndex = 0;
+    let isTuningActive = false; // Utilisez une variable pour suivre l'état de l'option de tuning
+    const productImages = document.querySelectorAll('#product-carousel .carousel-item');
+    const tunedCarImages = document.querySelectorAll('.tuned-car-image'); // Sélectionnez toutes les images avec la classe "tuned-car-image"
+    const totalImages = productImages.length;
+
+    const prevButton = document.getElementById('prev-button');
+    const nextButton = document.getElementById('next-button');
+    const tuningButton = document.getElementById('tuning-button');
+    const engineStageSelect = document.getElementById('engine-stage');
+    const productPriceElement = document.getElementById('product-price');
+
+
+    function updateVisibleImage() {
+        if (isTuningActive) {
+            productImages.forEach((img) => {
+                img.style.display = 'none'; // Masquez les images principales
+            });
+            tunedCarImages.forEach((img, index) => {
+                img.style.display = index === currentImageIndex ? 'block' : 'none'; // Affichez les images de tuning en fonction de l'index courant
+            });
+        } else {
+            productImages.forEach((img, index) => {
+                img.style.display = index === currentImageIndex ? 'block' : 'none'; // Affichez les images principales en fonction de l'index courant
+            });
+            tunedCarImages.forEach((img) => {
+                img.style.display = 'none'; // Masquez les images de tuning
+            });
+        }
+    }
+
+    function calculatePrice() {
+        const selectedStage = engineStageSelect.value;
+        let stagePriceIncrease;
+        let basePrice = Number.parseInt(document.querySelector("#product-price").title);
+        console.log(basePrice, document.querySelector("#product-price"));
+        switch (selectedStage) {
+            case 'stage0':
+                stagePriceIncrease = 0;
+                break;
+            case 'stage1':
+                stagePriceIncrease = 5000;
+                break;
+            case 'stage2':
+                stagePriceIncrease = 20000;
+                break;
+            case 'stage3':
+                stagePriceIncrease = 30000;
+                break;
+            default:
+                stagePriceIncrease = 0;
+        }
+
+        const newPrice = isTuningActive ? basePrice : (basePrice + stagePriceIncrease);
+        productPriceElement.textContent = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(newPrice);
+    }
+
+    prevButton.addEventListener('click', () => {
+        if (isTuningActive) {
+            currentImageIndex = (currentImageIndex - 1 + tunedCarImages.length) % tunedCarImages.length; // Défilement des images de tuning
+        } else {
+            currentImageIndex = (currentImageIndex - 1 + totalImages) % totalImages; // Défilement des images principales
+        }
+        updateVisibleImage();
+    });
+
+    nextButton.addEventListener('click', () => {
+        if (isTuningActive) {
+            currentImageIndex = (currentImageIndex + 1) % tunedCarImages.length; // Défilement des images de tuning
+        } else {
+            currentImageIndex = (currentImageIndex + 1) % totalImages; // Défilement des images principales
+        }
+        updateVisibleImage();
+    });
+
+    tuningButton.addEventListener('click', () => {
+        isTuningActive = !isTuningActive; // Inverser l'état de l'option de tuning
+        currentImageIndex = 0; // Réinitialisez l'index lorsque vous activez l'option de tuning
+        updateVisibleImage();
+        calculatePrice(); // Recalculer le prix en fonction de l'option de tuning
+    });
+
+    engineStageSelect.addEventListener('change', calculatePrice);
+
+    updateVisibleImage();
+    calculatePrice();
+});
